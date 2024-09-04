bd66c3f3e104d782402bcf767b37873b2540a343
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-09-04 23:38:26 +0530

    +1

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 
 int main(){
-    int firstNumber;
-    cin (firstNumber) << "Enter First Operand ?\n" 
+    int a = 5, b = 2;
+    float c = 5.0f;
+    cout << "sum = " << (a + b) << "\n";
+    cout << "difference = " << (a - b) << "\n"; 
+    cout << "product = " << (a * b) << "\n"; 
+    cout << "quotient = " << (c / b) << "\n"; 
+    cout << "Remainder = " << (a % b) << "\n"; 
+    cout << "typecast  = " << ((float)5/(int)2) << "\n"; 
     return 0;
 }
