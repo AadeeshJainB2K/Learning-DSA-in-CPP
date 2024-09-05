1ae1aeb0661861839ca748a53f876800c96ddc76
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-09-05 10:54:46 +0530

    +1

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-    int a = 5, b = 2;
-    float c = 5.0f;
-    cout << "sum = " << (a + b) << "\n";
-    cout << "difference = " << (a - b) << "\n"; 
-    cout << "product = " << (a * b) << "\n"; 
-    cout << "quotient = " << (c / b) << "\n"; 
-    cout << "Remainder = " << (a % b) << "\n"; 
-    cout << "typecast  = " << ((float)5/(int)2) << "\n"; 
+ 
+ int main () {
+    double firstNumber;
+    double operation;
+    double secondNumber;
+    cout << ("Enter first operand ? ");
+    cin >> (firstNumber);
+    cout << ("Enter Operation ? ");
+    cin >> (operation);
+    cout << ("Enter second operand ? ");
+    cin >> (secondNumber);
+    cout << (firstNumber && operation && secondNumber);
     return 0;
-}
+ }
---
+++ Rough/Rough.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    cout << ((3 > 5) || (2<10)) << ("\n");
+    return 0;
+}
+
