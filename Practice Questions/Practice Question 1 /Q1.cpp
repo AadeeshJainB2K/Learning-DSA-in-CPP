7b11d3493d8d44a87b89905246ff33c909d04d1b
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-09-05 11:07:24 +0530

    Completed my first question

---
+++ Practice Questions/Practice Question 1 /Q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std ;
+
+int main () {
+    int a,b;
+    cout << "Enter The First Number ? ";
+    cin >> (a);
+    cout << "Enter The Second Number ? ";
+    cin >> (b);
+    cout << ("The sum of the given numbers is = ") << (a + b) << ("\n");
+}
