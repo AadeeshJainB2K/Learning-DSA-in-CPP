b969ba4f53ee206958e476c2ffe030343c348ddf
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-09-05 14:24:19 +0530

    +1

---
+++ Practice Questions/Practice Question 3/Q3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a = 1;
+    if (a>=0) {
+        cout << (a) << " is positive\n";
+    }else if (a<=-1) {
+        cout << (a) << " is negative\n";
+    }
+    return 0;
+}
