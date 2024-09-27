b4bdba2bce02240146807be2d2a61b720a327712
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-09-27 20:36:16 +0530

    +1

--- Practice Questions/Practice Question 6/By For Loop/Q6.cpp
+++ Practice Questions/Practice Question 6/By For Loop/Q6.cpp
@@ -2,20 +2,14 @@
 using namespace std;
 
 int main() {
-    int sum = 0, n;
-    cout << ("Enter an number to see the sum of odd numbers btw one and the number : ");
-    cin >> (n);
-    for (int i = 0; i <= n; i++) {
-        if (i % 2 == 0) {
-            sum += i;
-            cout << (sum) << ("\n");
-        }
-        else if (i % 2 == !0) {
-            n++;
-            sum += i;
-            cout << (sum) << ("\n");
+    int number, sum;
+    cout << "Enter a number to chek sum of odd numbers btw 0 to the number you have entered";
+    cin >> number;
+    for (int i = 0; i <= number; i++) {
+        if (i % 2 == !0) {
+            sum = sum + i;
         }
     }
-    cout << (sum) << ("\n");
+    cout << (sum);
     return 0;
 }
