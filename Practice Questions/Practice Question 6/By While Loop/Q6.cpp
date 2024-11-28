ac1aeccf6e09b5deedfd17ad5aaae5010080d5b6
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-11-28 14:27:25 +0530

    completed q6

--- Practice Questions/Practice Question 6/By While Loop/Q6.cpp
+++ Practice Questions/Practice Question 6/By While Loop/Q6.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
-    int oddSum, upperLimit, i;
-    oddSum, i = 0;
-
+    int oddSum, number, i;
+    oddSum = 0;
+    i = 0;
+    number = 0;
+    cout << "Enter the upper limit to calculate the sum of odd numbers: ";
+    cin >> number;
+    while (i <= number) {
+        if (i % 2 != 0) {
+            oddSum = oddSum + i;
+        }
+        i++;
+    }
+    cout << (oddSum) << "\n";
 }
