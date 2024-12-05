c1bea33999cc24ceab851cce7a140070153b4da0
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-12-05 14:38:24 +0530

    +1

--- Practice Questions/Practice Question 7/Optimised Code/Q7.cpp
+++ Practice Questions/Practice Question 7/Optimised Code/Q7.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
-    
+    int n;
+    bool isPrime = true;
+    cout << "Enter a number to check if it is a prime number or not : ";
+    cin >> (n);
+    if (n <= 1) isPrime = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        cout << (n) << " is prime" << "\n";
+    }
+    else {
+        cout << (n) << " is composite" << "\n";
+    }
     return 0;
 }
