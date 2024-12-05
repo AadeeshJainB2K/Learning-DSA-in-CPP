158de07790db97a5d25d4585819128f0a095b57f
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-12-05 17:28:35 +0530

    +1

--- Practice Questions/Practice Question 8/Q8.cpp
+++ Practice Questions/Practice Question 8/Q8.cpp
@@ -1,6 +1,20 @@
-#incude <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-
+    int n, sum;
+    sum = 0;
+    cout << ("enter n ");
+    cin >> (n);
+    if (n > 0) {
+        cout << ("0");
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                cout << (" + ") << (i);
+                sum = sum + i;
+            }
+        }
+    }
+    cout << (" = ") << (sum) << ("\n");
+    return 0;
 }
