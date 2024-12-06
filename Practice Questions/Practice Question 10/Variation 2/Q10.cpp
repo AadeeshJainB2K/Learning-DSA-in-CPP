6bffda3ed116be7917d2d04d1e16027902e2a3db
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-12-06 17:38:26 +0530

    +1

--- Practice Questions/Practice Question 10/Variation 2/Q10.cpp
+++ Practice Questions/Practice Question 10/Variation 2/Q10.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
     int n = 4;
-    for (int i = 1; i <= 4; i++) {
+    for (int i = 0; i <= n - 1; i++) {
         char ch = 'A';
-        for (int i = 1;i <= n; i++) {
+        for (int i = 0;i <= n - 1; i++) {
             cout << (ch);
             ch += 1;
         }
