8f65e2a6ece3b5a9e388c15cff1305cecd76df9d
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-12-06 17:44:43 +0530

    added the return 0 ; because it was a int function

--- Practice Questions/Practice Question 11/Variation 1 /Q11.cpp
+++ Practice Questions/Practice Question 11/Variation 1 /Q11.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
+    int n = 3;
+    int m = n * n;
+    for (int i = 0; i <= n - 1; i++) {
+        for (int j = 0; j <= m; j++) {
+            cout(j);
+        }
+        cout << ("\n");
+    }
     return 0;
 }
