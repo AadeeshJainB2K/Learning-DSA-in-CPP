58372a02f00c00da73efca5171060772cba6224e
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-12-06 17:37:12 +0530

    +1

--- Rough/Rough.cpp
+++ Rough/Rough.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main() {
     int n = 4;
-    for (int i = 0; i <= 4; i++) {
-        
+    for (int i = 1; i <= 4; i++) {
+        char ch = 'A';
+        for (int i = 1;i <= n; i++) {
+            cout << (ch);
+            ch += 1;
+        }
+        cout << ("\n");
     }
 }
