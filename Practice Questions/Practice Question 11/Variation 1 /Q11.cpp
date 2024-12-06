e2aa521cf48c6f5b16b214e9eeeae3496ce24b56
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-12-06 17:52:38 +0530

    +1

--- Practice Questions/Practice Question 11/Variation 1 /Q11.cpp
+++ Practice Questions/Practice Question 11/Variation 1 /Q11.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main() {
     int n = 3;
-    int m = n * n;
+    int m = 1;
     for (int i = 0; i <= n - 1; i++) {
-        for (int j = 0; j <= m; j++) {
-            cout(j);
+        for (int j = 0; j <= n - 1; j++) {
+            cout << (m);
+            m++;
         }
         cout << ("\n");
     }
