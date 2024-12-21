8990f303573ed4001918b9de40989b62bfbfb0b0
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-12-21 13:55:42 +0530

    fixed a bug

--- Practice Questions/Practice Question 12/Variation 1 /Q12.cpp
+++ Practice Questions/Practice Question 12/Variation 1 /Q12.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int n = 4;
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - 1 - i; j++) {
             cout << (" ");
         }
