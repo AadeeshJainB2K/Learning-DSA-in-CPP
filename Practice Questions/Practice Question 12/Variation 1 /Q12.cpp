a0179d4c94df7ae5388dd6e3cc213e7e9826cebc
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-12-21 13:52:38 +0530

    completed piramid question

--- Practice Questions/Practice Question 12/Variation 1 /Q12.cpp
+++ Practice Questions/Practice Question 12/Variation 1 /Q12.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-    
+    int n = 4;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            cout << (" ");
+        }
+        for (int j = 0; j < i + 1;j++) {
+            cout << (j + 1);
+        }
+        for (int j = i; j > 0; j--) {
+            cout << (j);
+        }
+        cout << ("\n");
+    }
 }
