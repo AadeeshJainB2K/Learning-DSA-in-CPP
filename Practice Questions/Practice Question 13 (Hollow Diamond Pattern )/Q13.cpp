42bba6060d222d42a0ca33b463d8ae08761f8ecf
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-12-21 14:30:21 +0530

    Completed the diamond problem half

--- Practice Questions/Practice Question 13 (Hollow Diamond Pattern )/Q13.cpp
+++ Practice Questions/Practice Question 13 (Hollow Diamond Pattern )/Q13.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
+    int n = 4;
+    for (int i = 0; i < n;i++) {
+        for (int j = n - 1 - i; j > 0; j--) {
+            cout << ("  ");
+        }
+
+        for (int j = 0; j < 1; j++) {
+            cout << (" *");
+        }
+
+        for (int j = 0; j < 2 * i - 1; j++) {
+            cout << ("  ");
+        }
+
+
+        for (int j = 0; j < 1 && i > 0; j++) {
+            cout << (" *");
+        }
+
+
+        cout << ("\n");
+    }
     return 0;
 }
