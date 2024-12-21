9b6d1ab57014f69a855a577b6263f3fa058397ce
AadeeshJainB2K <aadeeshjain15@gmail.com>
2024-12-21 13:58:51 +0530

    +1

--- Practice Questions/Practice Question 11/Variation 11/Q11.cpp
+++ Practice Questions/Practice Question 11/Variation 11/Q11.cpp
@@ -14,4 +14,5 @@ int main() {
         ch++;
         cout << ("\n");
     }
+    return 0;
 }
--- Practice Questions/Practice Question 12/Q12.cpp
+++ Practice Questions/Practice Question 12/Q12.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int main() {
     int n = 4;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < n - 1 - i; j++) {
             cout << (" ");
         }
-        for (int k = 0; k < n - i; k++) {
-            cout << (i + 1);
+        for (int j = 0; j < i + 1;j++) {
+            cout << (j + 1);
+        }
+        for (int j = i; j > 0; j--) {
+            cout << (j);
         }
         cout << ("\n");
     }
