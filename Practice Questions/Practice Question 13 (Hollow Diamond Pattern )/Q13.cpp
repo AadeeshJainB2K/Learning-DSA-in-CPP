923b0d9b48ec4b1a7bd905ac6f058d767393b747
AadeeshJainB2K <aadeeshjain15@gmail.com>
2025-03-23 17:44:25 +0530

    finally revived for coding after 10th boards

--- Practice Questions/Practice Question 13 (Hollow Diamond Pattern )/Q13.cpp
+++ Practice Questions/Practice Question 13 (Hollow Diamond Pattern )/Q13.cpp
@@ -3,28 +3,36 @@ using namespace std;
 
 int main() {
     int n = 4;
-    for (int i = 0; i < n;i++) {
-
-        for (int j = n - 1 - i; j > 0; j--) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
             cout << ("  ");
         }
-
-        for (int j = 0; j < 1; j++) {
-            cout << (" *");
+        for (int j = 0; j < 1;j++) {
+            cout << ("*");
         }
-
-        for (int j = 0; j < 2 * i - 1; j++) {
+        for (int j = 1; j < 2 * i + 1; j++) {
+            cout << (" ");
+        }
+        for (int j = 0; j < i; j++) {
             cout << ("  ");
         }
-
-
-        for (int j = 0; j < 1 && i > 0; j++) {
-            cout << (" *");
+        if (i != 0) {
+            for (int j = 0; j < 1;j++) {
+                cout << ("*");
+            }
         }
-
-
         cout << ("\n");
     }
-
-    return 0;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j <= i + 1; j++) {
+            cout << ("  ");
+        }
+        for (int j = 0; j < 1;j++) {
+            cout << ("*");
+        }
+        for (int j = 0; j < n - i; j++) {
+            cout << ("a");
+        }
+        cout << ("\n");
+    }
 }
