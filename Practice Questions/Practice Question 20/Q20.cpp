80e343d2ea9a5b669492ae9575a6b272152e77bc
AadeeshJainB2K <aadeeshjain15@gmail.com>
2025-06-10 21:41:26 +0530

    completed the prime no homework question

--- Practice Questions/Practice Question 20/Q20.cpp
+++ Practice Questions/Practice Question 20/Q20.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    return 0;
+bool isPrime(int n) {
+    bool isPrime;
+    for (int i = 1; i < n; i++) {
+        if (n % i * i != 0) {
+            isPrime = true;
+        }
+        else {
+            isPrime = false;
+        }
+    }
+    return isPrime;
 }
 
-    
+int main() {
+    int number;
+    cout << "Enter a positive integer: ";
+    cin >> number;
+
+    if (isPrime(number) == 1) {
+        cout << number << " is a prime number." << endl;
+    }
+    else {
+        cout << number << " is not a prime number." << endl;
+    }
+
+    return 0;
+}
