c4f54581bc4365d636a98d09977997528c7f8b62
AadeeshJainB2K <aadeeshjain15@gmail.com>
2025-06-10 14:56:15 +0530

    +1

--- Practice Questions/Practice Question 18/Q18.cpp
+++ Practice Questions/Practice Question 18/Q18.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
-    return 0;
+int sumOfDigits(num) {
+    int sumOfDigits = 0;
+    while (num < 0) {
+        sumOfDigits += num % 10;
+        num /= 10;
+        return sumOfDigits;
+    }
 }
 
+int main() {
+    int num = 0;
+    int sumOfDigits = 0;
+    cout << ("Enter any number to calculate its sum of digits");
+    cin >> (num);
+    while (num < 0) {
+        sumOfDigits += num % 10;
+        num /= 10;
+        return sumOfDigits;
+    }
+    cout << (sumOfDigits);
+    return 0;
+}
