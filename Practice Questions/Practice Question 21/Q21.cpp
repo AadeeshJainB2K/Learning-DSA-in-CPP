b8376c2185a6bde28307174438c89e8ccc32f2c5
AadeeshJainB2K <aadeeshjain15@gmail.com>
2025-06-16 08:47:41 +0530

    +1

--- Practice Questions/Practice Question 21/Q21.cpp
+++ Practice Questions/Practice Question 21/Q21.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 
 int allPrimesTill(int n) {
-    for (int i = 0; i <= n; i++) {
-
+    for (int i = 1; i < n; i++) {
+        bool isPrime = false;
+        if (n % (i * i) != 0) {
+            isPrime = true;
+        }
+        if (isPrime == true) {
+            cout << i << " ";
+            isPrime = false;
+        }
     }
+    return 0;
 }
 
 
 int main() {
-
+    int n = 10;
+    cout << (allPrimesTill(n));
 }
