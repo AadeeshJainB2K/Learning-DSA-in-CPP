764c70ace29b7c03fb8bf949c41bc85b2ba08990
AadeeshJainB2K <aadeeshjain15@gmail.com>
2025-09-07 21:43:37 +0530

    Completed the fibonacci series question

--- Practice Questions/Practice Question 22/Q22.cpp
+++ Practice Questions/Practice Question 22/Q22.cpp
@@ -3,8 +3,17 @@ using namespace std;
 
 // Print Fibonacci Series till n ;
 
-
+void fibonnaciTill(int n) {
+    int firstTerm = 0;
+    int secondTerm = 1;
+    for (int i = 0; i < n; i++) {
+        cout << firstTerm << " ";
+        int thirdTerm = firstTerm + secondTerm;
+        firstTerm = secondTerm;
+        secondTerm = thirdTerm;
+    }
+}
 
 int main() {
-
+    fibonnaciTill(10);
 }
