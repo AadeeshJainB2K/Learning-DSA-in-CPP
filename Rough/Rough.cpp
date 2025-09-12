7493c9fdbcb8c12a449ce9ebb9261a3facb686eb
Aadeesh Jain <aadeeshjain15@gmail.com>
2025-09-12 17:05:08 +0530

    Revised all the logics of previous questions in rough

--- Rough/Rough.cpp
+++ Rough/Rough.cpp
@@ -1,20 +1,41 @@
 #include <iostream> 
 using namespace std;
 
-int main() {
-    int a, b;
-    cout << ("Enter first number");
-    cin >> a;
-    cout << ("enter second number");
-    cin >> b;
-
-    if (a > b) {
-        cout << (a) << (" is greater than ") << (b) << ("\n");
-
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
     }
-    else if (a < b) {
-        cout << (a) << (" is smaller than ") << (b) << ("\n");
+    for (int i = 2; i * i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
     }
-
+    return true;
 }
 
+int decimalToBinary(int n) {
+    int answer = 0, power = 1;
+    for (int i = 0; n > 0; i++) {
+        int remainder = n % 2;
+        answer += remainder * power;
+        power *= 10;
+        n /= 2;
+    }
+    return answer;
+}
+
+int binaryToDecimal(int n) {
+    int answer = 0, power = 1;
+    for (int i = 0; n > 0; i++) {
+        int remainder = n % 10;
+        answer += remainder * power;
+        power *= 2;
+        n /= 10;
+    }
+    return answer;
+}
+
+int main() {
+    cout << (binaryToDecimal(1010));
+    return 0;
+}
