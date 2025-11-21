ae1e71a15e93d15d59caeaa2dce665c4bad3a982
Aadeesh Jain <aadeeshjain15@gmail.com>
2025-11-21 19:55:39 +0530

    +1

--- Rough/Rough.cpp
+++ Rough/Rough.cpp
@@ -1,41 +1,17 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
-bool isPrime(int n) {
-    if (n <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i < n; i++) {
-        if (n % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int decimalToBinary(int n) {
-    int answer = 0, power = 1;
-    for (int i = 0; n > 0; i++) {
-        int remainder = n % 2;
-        answer += remainder * power;
-        power *= 10;
-        n /= 2;
-    }
-    return answer;
-}
-
-int binaryToDecimal(int n) {
-    int answer = 0, power = 1;
-    for (int i = 0; n > 0; i++) {
-        int remainder = n % 10;
-        answer += remainder * power;
-        power *= 2;
-        n /= 10;
-    }
-    return answer;
-}
-
 int main() {
-    cout << (binaryToDecimal(1010));
-    return 0;
+    int a = 5;
+    int b = 7;
+    cout << "Enter the first number: ";
+    //cin >> a;
+    cout << "Enter the second number: ";
+    //cin >> b;
+    cout << (a) << (b);
+    // swapping without the temp variable 
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << (a) << (b);
 }
