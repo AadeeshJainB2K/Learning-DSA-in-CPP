787ef76357b7b4d1a90a64086a59d9737cc50500
Aadeesh Jain <aadeeshjain15@gmail.com>
2025-11-24 23:20:06 +0530

    solved a question related to check if a number is a power of two or not

---
+++ Practice Questions/Practice Question 26/Approcah 1/Q26.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std ;
+
+int main (){
+    int n ;
+    int power = 0 ;
+    bool isPowerOf2 = true ;
+    cout << ("Enter a number to check if its a power of 2 : ");
+    cin >> n ;
+    for (int i = n ; i >= 1 ; i--){
+        if (n%2 != 0){
+            isPowerOf2 = false ;
+            break;
+        }else {
+        n/=2 ;
+        power++;
+        }
+    }
+    if (isPowerOf2 == true) {
+    cout << ("Yup it is the 2 to the power ")<< (power) << ("\n");
+    }else if (isPowerOf2 == false) {
+    cout << "No :( " << n << " is NOT a power of 2\n";
+    }
+}
